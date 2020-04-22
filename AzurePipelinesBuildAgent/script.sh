2ea0abc8f303dce44c1a7529710d500aa88232ca
oYPIo <37488187+oYPIo@users.noreply.github.com>
2020-04-22 21:16:46 +0300

    asdfsada

--- AzurePipelinesBuildAgent/script.sh
+++ AzurePipelinesBuildAgent/script.sh
@@ -10,7 +10,7 @@ sudo ./build-tools.sh
 curl https://raw.githubusercontent.com/MicrosoftDocs/mslearn-azure-pipelines-build-agent/master/build-agent.sh > build-agent.sh
 export AZP_AGENT_NAME=MyLinuxAgent
 export AZP_URL=https://dev.azure.com/Tailspin051
-export AZP_TOKEN=kggrcqw537qhqqf2c6wsutj7i53pdkbt2nwbfmfmajq3httpjsoa
+export AZP_TOKEN=3ti4aadaprdm5ppdz4vncwifcdhsdqajbvagcsonbz3pzxfnyrza
 export AZP_POOL=MyAgentPool
 export AZP_AGENT_VERSION=$(curl -s https://api.github.com/repos/microsoft/azure-pipelines-agent/releases | jq -r '.[0].tag_name' | cut -d "v" -f 2)
 chmod u+x build-agent.sh
