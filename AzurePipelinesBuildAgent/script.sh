55237c0f201f587931d960166196d3618684dbbe
oYPIo <37488187+oYPIo@users.noreply.github.com>
2020-04-22 22:41:14 +0300

    Add .bashrc with parameters for script.sh

--- AzurePipelinesBuildAgent/script.sh
+++ AzurePipelinesBuildAgent/script.sh
@@ -8,10 +8,10 @@ sudo ./build-tools.sh
 
 #install agent software
 curl https://raw.githubusercontent.com/MicrosoftDocs/mslearn-azure-pipelines-build-agent/master/build-agent.sh > build-agent.sh
-export AZP_AGENT_NAME=MyLinuxAgent
-export AZP_URL=https://dev.azure.com/Tailspin051
-export AZP_TOKEN=3ti4aadaprdm5ppdz4vncwifcdhsdqajbvagcsonbz3pzxfnyrza
-export AZP_POOL=MyAgentPool
-export AZP_AGENT_VERSION=$(curl -s https://api.github.com/repos/microsoft/azure-pipelines-agent/releases | jq -r '.[0].tag_name' | cut -d "v" -f 2)
+#install parameters for agent
+curl https://raw.githubusercontent.com/oYPIo/ARMTemplatesLabs/dev/buildAgent/AzurePipelinesBuildAgent/agentParameters.bashrc > agentParameters.bashrc
+chmod u+x agentParameters.bashrc
+sudo ./agentParameters.bashrc
+
 chmod u+x build-agent.sh
 sudo -E ./build-agent.sh
