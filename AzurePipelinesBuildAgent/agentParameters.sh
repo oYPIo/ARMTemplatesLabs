6a87543b507b2bfd522dfdc73a444a934bfb89fb
oYPIo <37488187+oYPIo@users.noreply.github.com>
2020-04-23 23:46:50 +0300

    sdfasfd

--- AzurePipelinesBuildAgent/agentParameters.sh
+++ AzurePipelinesBuildAgent/agentParameters.sh
@@ -1,5 +1,5 @@
 export AZP_AGENT_NAME=MyLinuxAgent
 export AZP_URL=https://dev.azure.com/Tailspin051
-export AZP_TOKEN=2lxk3hd577f2odzyx2ou3mtnkxrbgqunvy2spet2kvidpjy6ikqq
+export AZP_TOKEN=or4lpyvm22bg6gtbgnwnvmu7j3pfpzd6ypqh23jdgt5zmsblcduq
 export AZP_POOL=MyAgentPool
 export AZP_AGENT_VERSION=$(curl -s https://api.github.com/repos/microsoft/azure-pipelines-agent/releases | jq -r '.[0].tag_name' | cut -d "v" -f 2)
