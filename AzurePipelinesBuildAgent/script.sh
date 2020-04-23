2d0854a61a9e689beae5105aa1ddb04dec826e1f
oYPIo <37488187+oYPIo@users.noreply.github.com>
2020-04-23 19:38:40 +0300

    Add env var via .bashrc

--- AzurePipelinesBuildAgent/script.sh
+++ AzurePipelinesBuildAgent/script.sh
@@ -8,10 +8,5 @@ sudo ./build-tools.sh
 
 #install agent software
 curl https://raw.githubusercontent.com/MicrosoftDocs/mslearn-azure-pipelines-build-agent/master/build-agent.sh > build-agent.sh
-#install parameters for agent
-curl https://raw.githubusercontent.com/oYPIo/ARMTemplatesLabs/dev/buildAgent/AzurePipelinesBuildAgent/agentParameters.bashrc > agentParameters.bashrc
-chmod u+x agentParameters.bashrc
-sudo ./agentParameters.bashrc
-
 chmod u+x build-agent.sh
 sudo -E ./build-agent.sh
