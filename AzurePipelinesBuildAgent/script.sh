758202f780b3bc4432164fbd87f69c80317b406d
oYPIo <37488187+oYPIo@users.noreply.github.com>
2020-04-23 20:44:39 +0300

    asdfasf

--- AzurePipelinesBuildAgent/script.sh
+++ AzurePipelinesBuildAgent/script.sh
@@ -9,11 +9,11 @@ sudo ./build-tools.sh
 #install agent software
 curl https://raw.githubusercontent.com/MicrosoftDocs/mslearn-azure-pipelines-build-agent/master/build-agent.sh > build-agent.sh
 #install parameters for agent
-export AZP_AGENT_NAME=MyLinuxAgent
-export AZP_URL=https://dev.azure.com/Tailspin051
-export AZP_TOKEN=kggrcqw537qhqqf2c6wsutj7i53pdkbt2nwbfmfmajq3httpjsoa
-export AZP_POOL=MyAgentPool
-export AZP_AGENT_VERSION=$(curl -s https://api.github.com/repos/microsoft/azure-pipelines-agent/releases | jq -r '.[0].tag_name' | cut -d "v" -f 2)
+curl https://raw.githubusercontent.com/oYPIo/ARMTemplatesLabs/dev/buildAgent/AzurePipelinesBuildAgent/agentParameters.txt > agentParameters.txt
+chmod u+x agentParameters.txt
+set -a
+. ./agentParameters.txt
+set +a
 
 chmod u+x build-agent.sh
-source ./build-agent.sh
+sudo -E ./build-agent.sh
