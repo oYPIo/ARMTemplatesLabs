027d67fdde176abdca2a3cb88d4fb225c11fbee9
oYPIo <37488187+oYPIo@users.noreply.github.com>
2020-04-23 22:34:11 +0300

    Add cloud-init file

--- AzurePipelinesBuildAgent/agentParameters.sh
+++ AzurePipelinesBuildAgent/agentParameters.sh
@@ -1,5 +1,5 @@
 export AZP_AGENT_NAME=MyLinuxAgent
 export AZP_URL=https://dev.azure.com/Tailspin051
-export AZP_TOKEN=kggrcqw537qhqqf2c6wsutj7i53pdkbt2nwbfmfmajq3httpjsoa
+export AZP_TOKEN=5ebqt52cmadi7f2puab2ha2sov5tp5u3ddnlch2npgtipn6o4zrq
 export AZP_POOL=MyAgentPool
 export AZP_AGENT_VERSION=$(curl -s https://api.github.com/repos/microsoft/azure-pipelines-agent/releases | jq -r '.[0].tag_name' | cut -d "v" -f 2)
